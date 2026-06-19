cfd7cc6720696c635bb16fa9e6f4531ea3d4f448
David Puga <david.pugafudla.edu.ec>
2026-06-19 17:46:51 -0500

    Añadir datos

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,9 @@
 
 pip install -r requirements.txt
 
+python manage.py collectstatic --noinput
+
 python manage.py migrate
 
-python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
+
